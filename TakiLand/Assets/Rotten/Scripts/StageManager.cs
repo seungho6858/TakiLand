5a25563a68ac16fa645ffbd06fc1df1eb19120c1
quxn6 <quxn6@naver.com>
2025-01-25 02:14:57 +0900

    스테이지 넘기는 코드 작업

--- TakiLand/Assets/Rotten/Scripts/StageManager.cs
+++ TakiLand/Assets/Rotten/Scripts/StageManager.cs
@@ -1,13 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using EnumsNET;
 using Mib;
 using Mib.Data;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class StageManager : MonoSingleton<StageManager>
 {
+    public event Action<Formation, Formation> OnBattleStart;
+    
     protected override void OnAwake()
+    {
+        
+    }
+
+    public void EndBattle()
     {
         
     }
