3f5e89b01aa55b430e5d60aec50a7aef5439fcd3
quxn6 <quxn6@naver.com>
2025-01-25 18:17:11 +0900

    베팅내역 text 초기화빼먹은거올림

--- TakiLand/Assets/Rotten/UI/ScoreUI.cs
+++ TakiLand/Assets/Rotten/UI/ScoreUI.cs
@@ -16,6 +16,8 @@ public class ScoreUI : MonoBehaviour
     public void Initialize(int stage)
     {
         _stage = stage;
+        _resultText.text = string.Empty;
+        _goldText.text = string.Empty;
     }
 
     public void UpdateScore(bool isWin, int goldDelta)
