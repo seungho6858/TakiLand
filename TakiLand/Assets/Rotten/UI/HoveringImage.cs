e06321357049270ce0d19369a324c8c1200e1683
quxn6 <quxn6@naver.com>
2025-01-25 17:28:38 +0900

    호버 있는 버튼 반영

---
+++ TakiLand/Assets/Rotten/UI/HoveringImage.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Image))]
+public class HoveringImage : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+	private Image _targetImage; // 토글할 이미지
+
+	private void Awake()
+	{
+		_targetImage = GetComponent<Image>();
+		ChangeAlpha(0.0f);
+	}
+
+	// 마우스가 버튼 위에 올라갈 때 호출
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		ChangeAlpha(1.0f);
+	}
+
+	// 마우스가 버튼에서 벗어날 때 호출
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		ChangeAlpha(0.0f);
+	}
+
+	private void ChangeAlpha(float value)
+	{
+		Color color = _targetImage.color;
+		color.a = value;
+		_targetImage.color = color;
+	}
+}
